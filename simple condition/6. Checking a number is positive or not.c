4bbc1f00c06d05ede3063ccca3c36ddd51346a71
Fsadia2610 <71710806+Fsadia2610@users.noreply.github.com>
2022-09-12 09:47:06 +0600

    Add files via upload

---
+++ simple condition/6. Checking a number is positive or not.c
@@ -0,0 +1,16 @@
+/*This program determines whether a number is positive or not
+Author: Fatema Tuj Juhra Sadia(222-16-667)*/
+#include<stdio.h>
+int main(){
+float number;
+
+printf("Please enter a number = ");
+scanf("%f",&number);
+
+if(number>0){
+    printf("This number is positive");
+}else{
+printf("This number is not positive");
+}
+return 0;
+}
---
+++ simple condition/8. Checking a number is Zero(0) or not.c
@@ -0,0 +1,16 @@
+/*This program determines whether a number is Zero(0) or not
+Author: Fatema Tuj Juhra Sadia(222-16-667)*/
+#include<stdio.h>
+int main(){
+float number;
+
+printf("Please enter a number = ");
+scanf("%f",&number);
+
+if(number==0){
+    printf("This number is Zero(0)");
+}else{
+printf("This number is not Zero(0)");
+}
+return 0;
+}
